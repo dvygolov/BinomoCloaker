2d54e65ce6b6ff28ee88d5ea8af5f84f3ef07e8c
Kill Buddha <rattack@gmail.com>
2025-02-09 17:16:54 +0400

    other camp delete button icon

--- admin/tablecolumns.php
+++ admin/tablecolumns.php
@@ -112,7 +112,7 @@ function get_campaigns_columns(array $columns): string
             "formatter": function() {
                 return `
                     <button class="btn btn-camp btn-rename" title="Rename"><i class="bi bi-pencil-fill"></i></button>
-                    <button class="btn btn-camp btn-delete" title="Delete"><i class="bi bi-file-x"></i></button>
+                    <button class="btn btn-camp btn-delete" title="Delete"><i class="bi bi-trash-fill"></i></button>
                     <button class="btn btn-camp btn-clone" title="Clone"><i class="bi bi-copy"></i></button>
                     <button class="btn btn-camp btn-stats" title="View stats"><i class="bi bi-bar-chart-fill"></i></button>
                     <button class="btn btn-camp btn-allowed" title="View allowed clicks"><i class="bi bi-person-circle"></i></button>
